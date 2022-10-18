ab45d22d4d743ed4e22462647fcd2afdf8718cb2
tcjayswal-153799200 <tcjayswal@myseneca.ca>
2022-10-18 11:00:41 -0700

    Adding Lab1 files

---
+++ Lab1/lab1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#Author: Tirth Jayswal
+#Seneca Id= tcjayswal (153799200)
+#Date Created = 2022/10/17
+
+age=int(input('Please enter your age: '))
+print('Your age is ' + str(age))
+
